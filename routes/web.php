672775596ba3d54f498461c2f6eba7ed1c6ab173
adrianeh <adrianelhombrehernandez@gmail.com>
2023-12-21 15:22:53 +0100

    First application Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,21 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('chirps', ChirpController::class)
+->only(['index', 'store', 'edit', 'update', 'destroy'])
+->middleware(['auth', 'verified']);
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
